efa495a8fc1cc14ffd77d3c1db460235889a9fba
Derek <derekalia@me.com>
2017-10-22 11:35:53 -0700

    init commit

---
+++ webpack.server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+
+const config = {
+  // inform webpack that we're building a bundle for nodeJs rather than for browser
+
+  target: "node",
+
+  // tell webpack the root file of our server application:
+  entry: "./src/index.js",
+
+  // tell webpack where to put the output file thats generated
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  }
+};
+
+
+module.exports = merge(baseConfig,config);
