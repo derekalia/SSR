d0a419fe95dd93f79d117e02ac3ae4289a14e940
Derek Alia <derekalia@me.com>
2017-10-23 08:28:51 -0700

    adding redux

--- src/client/client.js
+++ src/client/client.js
@@ -3,7 +3,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import Routes from './Routes';
+
+const store = createStore(reducers)
 
 ReactDOM.hydrate(
   <BrowserRouter>
